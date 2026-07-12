bcf15c9d83789a8f7742ac345a76d99737a335e5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:05 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "kubernetes_fleet_update_runs" {
-  description = "All kubernetes_fleet_update_run resources"
-  value       = azurerm_kubernetes_fleet_update_run.kubernetes_fleet_update_runs
-}
 output "kubernetes_fleet_update_runs_fleet_update_strategy_id" {
-  description = "List of fleet_update_strategy_id values across all kubernetes_fleet_update_runs"
-  value       = [for k, v in azurerm_kubernetes_fleet_update_run.kubernetes_fleet_update_runs : v.fleet_update_strategy_id]
+  description = "Map of fleet_update_strategy_id values across all kubernetes_fleet_update_runs, keyed the same as var.kubernetes_fleet_update_runs"
+  value       = { for k, v in azurerm_kubernetes_fleet_update_run.kubernetes_fleet_update_runs : k => v.fleet_update_strategy_id }
 }
 output "kubernetes_fleet_update_runs_kubernetes_fleet_manager_id" {
-  description = "List of kubernetes_fleet_manager_id values across all kubernetes_fleet_update_runs"
-  value       = [for k, v in azurerm_kubernetes_fleet_update_run.kubernetes_fleet_update_runs : v.kubernetes_fleet_manager_id]
+  description = "Map of kubernetes_fleet_manager_id values across all kubernetes_fleet_update_runs, keyed the same as var.kubernetes_fleet_update_runs"
+  value       = { for k, v in azurerm_kubernetes_fleet_update_run.kubernetes_fleet_update_runs : k => v.kubernetes_fleet_manager_id }
 }
 output "kubernetes_fleet_update_runs_managed_cluster_update" {
-  description = "List of managed_cluster_update values across all kubernetes_fleet_update_runs"
-  value       = [for k, v in azurerm_kubernetes_fleet_update_run.kubernetes_fleet_update_runs : v.managed_cluster_update]
+  description = "Map of managed_cluster_update values across all kubernetes_fleet_update_runs, keyed the same as var.kubernetes_fleet_update_runs"
+  value       = { for k, v in azurerm_kubernetes_fleet_update_run.kubernetes_fleet_update_runs : k => v.managed_cluster_update }
 }
 output "kubernetes_fleet_update_runs_name" {
-  description = "List of name values across all kubernetes_fleet_update_runs"
-  value       = [for k, v in azurerm_kubernetes_fleet_update_run.kubernetes_fleet_update_runs : v.name]
+  description = "Map of name values across all kubernetes_fleet_update_runs, keyed the same as var.kubernetes_fleet_update_runs"
+  value       = { for k, v in azurerm_kubernetes_fleet_update_run.kubernetes_fleet_update_runs : k => v.name }
 }
 output "kubernetes_fleet_update_runs_stage" {
-  description = "List of stage values across all kubernetes_fleet_update_runs"
-  value       = [for k, v in azurerm_kubernetes_fleet_update_run.kubernetes_fleet_update_runs : v.stage]
+  description = "Map of stage values across all kubernetes_fleet_update_runs, keyed the same as var.kubernetes_fleet_update_runs"
+  value       = { for k, v in azurerm_kubernetes_fleet_update_run.kubernetes_fleet_update_runs : k => v.stage }
 }
 
